ee7c356b5b47ef840ac5241c326ae8b135cd5673
Poulami Bhattacharyya <poulamibhattacharyya77@gmail.com>
2026-03-24 02:06:29 +0530

    Create script.js

---
+++ 2. Functions/script.js
@@ -0,0 +1,13 @@
+Funcion in JavaScript are value, can be stored in variables, pass them around and return from another functions.
+
+function greet() {
+  console.log("Hi")
+};
+greet();
+# When the 'greet' funtion is executed it will print "Hi".
+
+let hello = function(){
+  console.log("Say Hello")
+};
+hello();
+# here function is stored in a variable which you can use later.
