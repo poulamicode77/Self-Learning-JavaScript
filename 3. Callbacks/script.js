7efd3681c29b5fc7c70d902f40c7a48cb091092b
Poulami Bhattacharyya <poulamibhattacharyya77@gmail.com>
2026-03-25 01:11:27 +0530

    Implement callback function for greeting
    Added a callback function to greet a user.

--- 3. Callbacks/script.js
+++ 3. Callbacks/script.js
@@ -1,1 +1,9 @@
-
+Callback function is passed inside a function to be exceuted later
+function greet(name){
+    console.log("Hello, " + name + "!") ;
+}
+function userInput(callback){
+    let name = "Poulami";
+    callback(name);
+}
+userInput(greet);
